7f7012a4db793fb99aa2092c2b05c2e8d1ac492c
imutae <imutae.lab@gmail.com>
2026-04-28 21:09:47 +0900

    갑자기 생긴 에러 수정중

--- PacketId.h
+++ PacketId.h
@@ -3,22 +3,22 @@
 
 enum class PacketId : std::uint16_t
 {
-    R_CHAT = 1,      // 로비 채팅 전송
-    S_CHAT = 2,      // 로비 채팅 수신
+    R_CHAT = 1,      
+    S_CHAT = 2,      
 
-    R_JOIN = 3,      // 방 생성 / 방 참가 요청
-    S_JOIN = 4,      // 방 생성 / 참가 결과 + 현재 방 플레이어 정보
+    R_JOIN = 3,      
+    S_JOIN = 4,      
 
-    R_MOVE = 5,      // 내 위치 전송
-    S_MOVE = 6,      // 같은 방 플레이어들에게 위치 전송
+    R_MOVE = 5,      
+    S_MOVE = 6,      
 
-    E_JOIN = 7,      // 같은 방의 다른 플레이어가 들어옴
-    E_LEAVE = 8,     // 같은 방의 다른 플레이어가 나감
+    E_JOIN = 7,      
+    E_LEAVE = 8,     
 
-	N_LEAVE = 9,     // 클라이언트 -> 서버 방 이탈 요청
+	N_LEAVE = 9,     
 
-    R_ROOM_LIST = 10, // 방 목록 요청
-    S_ROOM_LIST = 11, // 방 목록 응답
+    R_ROOM_LIST = 10,
+    S_ROOM_LIST = 11,
 
-    E_ACCEPT = 12,   // 접속 직후 내 SessionId 전달
+    E_ACCEPT = 12,   
 };
